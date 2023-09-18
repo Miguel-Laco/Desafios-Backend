d4df5839b3c2b5874462c8c310fd29e6b6776486
Miguel-Laco <magulaco@gmail.com>
2023-09-17 21:52:47 -0300

    DESAFÍO ENTREGABLE - Documentar API
    Consigna:
    Realizar la configuración necesaria para tener documentado tu proyecto final a partir de Swagger.
    
    Aspectos a incluir:
    Se debe tener documentado el módulo de productos.
    Se debe tener documentado el módulo de carrito
    No realizar documentación de sesiones

---
+++ 39. Documentacion de API/src/DAO/model/users.model.js
@@ -0,0 +1,18 @@
+/* users.model */
+import mongoose from "mongoose";
+import config from "../../config/config.js";
+
+const usersCollection = config.COLLECTION_USERS;
+
+const UserSchema = new mongoose.Schema({
+    first_name: String,
+    last_name: String,
+    email: {type: String, unique: true},
+    password: String,
+    age: Number,
+    cart: {type:mongoose.Schema.Types.ObjectId, ref: config.COLLECTION_CARTS},
+    role: {type: String, default: "user"}
+});
+
+
+export const usersModel = mongoose.model(usersCollection, UserSchema)
